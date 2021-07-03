741277641d6a73c0ce4e8d350a5249c84c8e8e32
Ana Back <74128856+Backatena@users.noreply.github.com>
2021-07-03 00:17:07 -0300

    Create questao2AtividadeApp.py

---
+++ questao2AtividadeApp.py
@@ -0,0 +1,14 @@
+
+a = float(input('digite um número: '))
+b = float(input('digite um número: '))
+c = float(input('digite um número: '))
+d = float(input('digite um número: '))
+
+media = (a + b + c + d) / 4
+
+print('A média é: ',media)
+
+if media < 0: 
+    print('negativo: ', media)
+elif media > 0: 
+    print('positivo: ', media)    
